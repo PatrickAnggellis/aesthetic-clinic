3f1cb1930a4fd903fb8cdb93537c7d1582998bb5
patrickAnggellis <patrickbananeiras@gmail.com>
2024-09-16 10:46:06 -0300

    Addition of folders related to hexagonal architecture

---
+++ src/main/java/br/com/clinic/aesthetic/AestheticClinicApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinic.aesthetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AestheticClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AestheticClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/clinic/aesthetic/AestheticClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinic.aesthetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AestheticClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
